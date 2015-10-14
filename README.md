45e9cdd7f4236171a5a65abf0a2288ef6b22b34a
Andrea Stagi <stagi.andrea@gmail.com>
2015-10-14 08:58:58 +0200

    First import

---
+++ README.md
@@ -0,0 +1,5 @@
+# ES2015 Samples
+
+## Compile the example
+
+    babel es6/main.js > js/main.js
---
+++ es6/main.js
@@ -0,0 +1,75 @@
+// 1. Classes
+class Person {
+  // 2. Default parameter
+  constructor(firstName, lastName, age=18) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._age = age;
+    this._friends = [];
+  }
+
+  getIdentityCard() {
+    // 5. Shorthand Object Literals
+    let name = this._firstName + ' ' + this._lastName;
+    let age = this._age;
+    return {name, age}
+  }
+
+  hasFriend(friend) {
+    for (let currentFriend in this._friends) {
+      if (this._friends[currentFriend] == friend) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addFriend(friend) {
+    if (!(this.hasFriend(friend))) {
+      this._friends.push(friend);
+    } else {
+      return;
+    }
+    friend.addFriend(this);
+  }
+
+  pullEar() {
+    let that = this;
+    var pullEarActions = {
+      [Symbol.iterator]: function*() {
+        for (let i = 1; i <= that._age; i++) {
+          yield i;
+        }
+      }
+    };
+    return pullEarActions;
+  }
+
+  showFriends() {
+    // 4. Arrows
+    this._friends.forEach(f =>
+      // 3. Template Strings
+      // 4. Unlike functions, arrows share the same lexical this as their surrounding code
+      console.log(`${this._firstName} and ${f._firstName} are friends`)
+    );
+  }
+}
+
+let andrea = new Person('Andrea', 'Stagi');
+let mark = new Person('Mark', 'Iozz');
+let matt = new Person('Matt', 'Lap');
+
+andrea.addFriend(mark);
+andrea.addFriend(mark);
+andrea.showFriends();
+mark.showFriends();
+
+for (var age of andrea.pullEar()) {
+  // truncate the sequence at 15
+  if (age == 15) {
+    break;
+  }
+  console.log(`${age}!!`);
+}
+
+console.log(mark.getIdentityCard());
